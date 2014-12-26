2dc86d7c55a290bbc97c3d65e43a696a9fcfdde4
Merrick Bautista <merrickbautista@gmail.com>
2014-12-26 00:02:07 -0600

    Made basic User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141226004906) do
+ActiveRecord::Schema.define(version: 20141226055344) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
